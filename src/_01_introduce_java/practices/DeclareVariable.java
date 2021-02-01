362fb4643ce9e5f08222aa38308a6cf2247ffe6e
C1220G1_NguyenDucHau <tieusau12@gmail.com>
2021-02-01 15:03:38 +0700

    up thuc hanh

---
+++ src/_01_introduce_java/practices/DeclareVariable.java
@@ -0,0 +1,18 @@
+package _01_introduce_java.practices;
+
+public class DeclareVariable {
+    public static void main(String[] args) {
+        int i =10;
+        float f =20.5f;
+        double b= 20.5;
+        boolean d = true;
+        char c = 'a';
+        String s = " Đà Nẵng";
+        System.out.println("i = " +i);
+        System.out.println("f = " +f);
+        System.out.println("b = " +b);
+        System.out.println("d = " +d);
+        System.out.println("c = " +c);
+        System.out.println("s = " +s);
+    }
+}
