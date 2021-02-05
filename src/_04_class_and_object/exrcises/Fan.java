afc07f491dac6cfa7f335fbb0a0cab9dccd826f4
MoonHZ <58355523+tieusau12@users.noreply.github.com>
2021-02-05 07:30:23 +0700

    up access modifier

---
+++ src/_04_class_and_object/exrcises/Fan.java
@@ -0,0 +1,78 @@
+package _04_class_and_object.exrcises;
+
+
+import java.util.Scanner;
+
+public class Fan{
+    final int SLOW = 1;
+    final int MEDIUM = 2;
+    final int FAST = 3;
+    private int speed = SLOW;
+    private boolean isOn = false;
+    private double radius = 5;
+    private String color = "Blue";
+    public Fan(){
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public boolean getIsOn(){
+        return isOn;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public String getColor() {
+        return color;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+    public void setOn(boolean on) {
+        this.isOn = on;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public String toString(){
+        if(getIsOn()){
+            return "Fan is on , Speed: " + getSpeed() + ", Color: " + getColor() + ", Radius: " + getRadius();
+        } else {
+            return "Fan is off , Radius: " + getRadius() + ", Color " + getColor();
+        }
+    }
+
+    public static void main(String[] args) {
+        Fan fan1 = new Fan();
+        Fan fan2 = new Fan();
+        fan1.setOn(true);
+        fan1.setColor("Yellow");
+        fan1.setRadius(10);
+        fan1.setSpeed(fan1.FAST);
+
+        fan2.setSpeed(fan2.MEDIUM);
+        Scanner sc = new Scanner(System.in);
+        int choose;
+        while (true){
+            System.out.println("1.Check Fan 1");
+            System.out.println("2.Check Fan 2");
+            System.out.println("3 To exit");
+            choose = sc.nextInt();
+            switch (choose){
+                case 1:
+                    System.out.println(fan1.toString() + "\n");
+                    break;
+                case 2:
+                    System.out.println(fan2.toString() + "\n");
+                    break;
+                case 3:
+                    System.exit(0);
+            }
+        }
+    }
+}
+
--- src/_04_class_and_object/exrcises/StopWatch.java
+++ src/_04_class_and_object/exrcises/StopWatch.java
@@ -1,18 +1,54 @@
 package _04_class_and_object.exrcises;
 
 import java.util.Date;
+import java.util.Random;
 
 public class StopWatch {
-    private Date startTime, endTime;
+    private long startTime, endTime;
 
-    public Date getEndTime() {
+    public long getEndTime() {
         return endTime;
     }
 
-    public Date getStartTime() {
+    public long getStartTime() {
         return startTime;
     }
-    public StopWatch (){
 
+    public long start() {
+        System.out.println(java.time.LocalTime.now());
+        return this.startTime = System.currentTimeMillis();
     }
+
+    public long stop() {
+        System.out.println(java.time.LocalTime.now());
+        return this.endTime = System.currentTimeMillis();
+    }
+
+    public long getElapsedTime() {
+        return getEndTime() - getStartTime();
+    }
+
+    public static void main(String[] args) {
+        StopWatch stopWatch = new StopWatch();
+        int[] array = new int[100000];
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(10000);
+        }
+        stopWatch.start();
+        for (int j = 0; j < array.length; j++) {
+            int min = array[j];
+            for (int z = j + 1; z < array.length; z++) {
+                if (min > array[z]) {
+                    int swap = min;
+                    min = array[z];
+                    array[z] = swap;
+                }
+            }
+        }
+        stopWatch.stop();
+        System.out.println("Time to sort: " + stopWatch.getElapsedTime() + " Milisecond");
+    }
+
 }
+
